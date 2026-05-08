f7e741ba1869366b8f7df1db7c8d01aade9ba743
decky254 <derricknjenga17@gmail.com>
2026-05-09 00:05:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 // TEST ROUTE
